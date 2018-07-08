575157dd4e585a875a7afb18c84692a677c83e88
Alex Rogerio <alexx.rogerio@gmail.com>
2018-07-08 13:35:59 -0300

    Criado top do site

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html >
+<head>
+	<title>Classificados</title>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/script.js"></script>
+</head>
+<body>
+	<nav class="navbar navbar-inverse">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<a href="./" class="navbar-brand">Classificados</a>
+			</div>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="">Cadastre-se</a></li>
+				<li><a href="">Login</a></li>
+			</ul>
+		</div>
+	</nav>
+</body>
+</html>
