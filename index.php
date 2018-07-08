a02db35252596fd35d25fd1493a9ccaefc185352
Alex Rogerio <alexx.rogerio@gmail.com>
2018-07-08 13:52:46 -0300

    Organizacao nas pasta e estrutura do arquivo

--- index.php
+++ index.php
@@ -1,26 +1,13 @@
-<!DOCTYPE html>
-<html >
-<head>
-	<title>Classificados</title>
-	<meta charset="utf-8"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" />
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
-	<script type="text/javascript" src="assets/js/jquery-3.3.1.min.js"></script>
-	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="assets/js/script.js"></script>
-</head>
-<body>
-	<nav class="navbar navbar-inverse">
-		<div class="container-fluid">
-			<div class="navbar-header">
-				<a href="./" class="navbar-brand">Classificados</a>
-			</div>
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="">Cadastre-se</a></li>
-				<li><a href="">Login</a></li>
-			</ul>
+<?php require 'pages/header.php';  ?>
+	<div class="container-fluid">
+		<div class="jumbotron">
+			<h2>Nós temos hoje 999 anúncios</h2>
+			<p>E mais de 999 usuários cadstrados</p>
 		</div>
-	</nav>
-</body>
-</html>
+		<div class="row">
+			<div class="col-sm-3"><h4>Pesquisa Avançada</div>
+			<div class="col-sm-9"><h4>Últimos Anúncios</h4></div>
+		</div>
+	</div>
+
+<?php require 'pages/footer.php';  ?>
